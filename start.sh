4558355e55f6c93334b5864055030af9a4854317
Raka Zidan <raka.zidan20@gmail.com>
2026-07-02 20:16:15 +0700

    fix: allow workflow access to environment variables

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # ponytail: map Railway's PORT to N8N_PORT, then delegate to official entrypoint
 export N8N_PORT="${PORT:-5678}"
+export N8N_BLOCK_ENV_ACCESS_IN_NODE=false
 exec /docker-entrypoint.sh "$@"
